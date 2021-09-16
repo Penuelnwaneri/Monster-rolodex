abc155b4439723d0a09ef96902ab582fc0db6111
Penuelnwaneri <penuelchudi@gmail.com>
2021-09-16 21:47:31 +0100

    destructured props of  card and cardlist components.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,11 @@
 import {Card} from '../card/card.component'
 import './card-list.style.css';
 
-export const CardList = (props) => {
+export const CardList = ({monsters}) => {
     return(
         <div className="card-list">
            {
-        props.monsters.map(monster => 
+            monsters.map(monster => 
           <Card monster={monster} key={monster.id}/>
 
         )
